668e40b71c1373bcd69a4d90b20ec8db22150352
andydeacon <andydeacon@merlsam.co.uk>
2021-04-22 11:29:51 +0100

    cleaning up code stage 1

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
+// create array to store 3 reviews that are selectable by the user
+// 01 is default
+
 const reviews = [
   {
       id: 1,
       quote: "Y&7’s research and user testing helped us identify key user pain points and in an extremely short time-to-market and launch a highly competitive offering.",
       author: "Marcus Richards",
       position: "Project Manager, Charity X",
-      img: "images/MarcusRichards.png"
+      img: "./images/MarcusRichards.png"
   },
   {
       id: 2,
       quote: "I cannot thank Y&7 enough for their expertise, they consistently deliver on their promises and have enabled our startup to thrive in an uncertain market.",
       author: "Lenny Hendridge",
       position: "CEO, My New Charity PLC",
-      img: "images/LennyHendridge.png"
+      img: "./images/LennyHendridge.png"
   },
   {
       id: 3,
       quote: "Y&7 worked on a campaign to recruit an army of good to help the needy in the face of an evil adversary.  Well done, you shall pass...",
       author: "Gandalf the grey",
       position: "Keeper of the Good",
-      img: "images/Gandalf.png"
+      img: "./images/Gandalf.png"
   }
 ];
-// select items
+
+// Following Javascript to demonstrate using the DOM
+// If this website was ever going to be released, this could be changed to jquery code
+// Partnering contact.js using jQuery to demonstrate knowledge of its use
+
+// get review elements from document and store as consts
 
 const quote = document.getElementById("quotetext");
 const author = document.getElementById("author");
 const position = document.getElementById("position");
 const img = document.getElementById("authorimg");
 
+// target 3 buttons and store in consts.  These allow the user to select from one of three reviews
+
 const btn1 = document.querySelector("#selling-point-container1");
 const btn2 = document.querySelector("#selling-point-container2");
 const btn3 = document.querySelector("#selling-point-container3");
 
-const sellBtns = document.querySelectorAll("sell-btn");
-
-const btnSubmit = document.querySelector(".submit-btn");
-
-// set starting items
+// set first as starting review
 let currentItem = 0;
 
 // load initial items
@@ -44,7 +50,7 @@ window.addEventListener("DOMContentLoaded", function () {
   showReview();
 });
 
-// show review based on items
+// function to display relevant information to the review selected
 
 function showReview() {
   const item = reviews[currentItem];
@@ -55,7 +61,8 @@ function showReview() {
 }
 
 
-// show next review
+// change color state of selected and unselected review buttons
+// Note for future development, would refactor to optimise code to shorter length
 
 btn1.addEventListener("click", function() {
   btn1.style.backgroundColor = "#FB7161";
@@ -81,6 +88,13 @@ btn3.addEventListener("click", function() {
   showReview();
 });
 
+// NEWSLETTER SIGN UP
+// target newsletter sign up submit button - Submitting a valid email triggers an alert to the user
+// there is no functionality beyond the alert and the user currently doesnt receive an email
+
+const btnSubmit = document.querySelector(".submit-btn");
+const emailAddress = document.querySelector(".email-address");
+
 btnSubmit.addEventListener("click", function() {
-  alert("You have been subscribed to our newsletter, you can unsubscribe any time by clicking the unsubscribe link from any correspondence. Close this message to continue.");
+  alert("An invitation email has been sent to " + emailAddress.value + ".  Follow the instructions on the email to confirm your subscription to our newletter.");
 });
